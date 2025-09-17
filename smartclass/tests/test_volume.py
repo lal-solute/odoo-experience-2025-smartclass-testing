5aa1f69a2d4e79b3c6779f080fb537e00c6a4504
Lars Liedtke <lal@solute.de>
2025-09-17 11:47:17 +0200

    python: write first tests

--- smartclass/tests/test_volume.py
+++ smartclass/tests/test_volume.py
@@ -9,6 +9,10 @@ class TestVolume(odoo.tests.HttpCase):
     def test_volume_model(self):
         # Create at leat 3 volumes and check that their volumes and categories are as expected.
         # Adapt function in your model to test that:
+        volume_zero = self.env["smartclass.volume"].create([{"name": "small", "depth": 0, "height": 0, "width": 0}])
+        volume_small_1 = self.env["smartclass.volume"].create([{"name": "small1", "depth": 0.95, "height": 0.95, "width": 0.95}])
+        volume_small_2 = self.env["smartclass.volume"].create([{"name": "small2", "depth": 0.995, "height": 0.995, "width": 0.995}])
+        volume_small_3 = self.env["smartclass.volume"].create([{"name": "small3", "depth": 1, "height": 1, "width": 1}])
         # - When volume is 0, category is small
         # - When volume is 0.86, category is small
         # - When volume is 0.99, category is small
@@ -17,6 +21,17 @@ class TestVolume(odoo.tests.HttpCase):
         # - When volume is 100, category is medium
         # - When volume is 101, category is large
         # - When volume is 1000 category is large
+        self.assertEqual(0, volume_zero.volume)
+        self.assertEqual("small", volume_zero.category)
+
+        self.assertEqual(0.86, volume_small_1.volume)
+        self.assertEqual("small", volume_small_1.category)
+
+        self.assertEqual(0.99, volume_small_2.volume)
+        self.assertEqual("small", volume_small_2.category)
+
+        self.assertEqual(1, volume_small_3.volume)
+        self.assertEqual("small", volume_small_3.category)
         return True
 
     def test_tour_create_volumes(self):
